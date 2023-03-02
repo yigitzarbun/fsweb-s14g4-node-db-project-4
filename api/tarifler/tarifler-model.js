907aa7eda8efad9156a741a845c00d058f5b1d2e
Yigit Zarbun <yzarbun@gmail.com>
2023-03-02 18:26:13 +0300

    mvp tamamlandı

--- api/tarifler/tarifler-model.js
+++ api/tarifler/tarifler-model.js
@@ -14,11 +14,20 @@ async function idyeGoreTarifGetir(tarif_id) {
   const icindekiler = await db("miktar")
     .leftJoin("tarifler", "tarifler.id", "miktar.tarifler_id")
     .leftJoin("icindekiler", "miktar.icindekiler_id", "icindekiler.id")
-    .select("icindekiler.icindekiler_adi")
+    .select("icindekiler.id", "icindekiler.icindekiler_adi")
     .where("tarifler.id", tarif_id);
   const miktar = await db("miktar")
     .leftJoin("tarifler", "tarifler.id", "miktar.tarifler_id")
-    .select("miktar.miktar")
+    .leftJoin("icindekiler", "icindekiler.id", "miktar.icindekiler_id")
+    .leftJoin("adimlar", "adimlar.id", "miktar.adimlar_id")
+    .select(
+      "tarifler.id as tarif_id",
+      "adimlar.id as adim_id",
+      "adimlar.adim_sirasi as adim_sirasi",
+      "icindekiler.id as icindekiler_id",
+      "icindekiler.icindekiler_adi as icindekiler_adi",
+      "miktar.miktar as miktar"
+    )
     .where("tarifler.id", tarif_id);
 
   const result = {
@@ -28,14 +37,29 @@ async function idyeGoreTarifGetir(tarif_id) {
     adimlar: [],
   };
 
-  const adimlarArray = adimlar.forEach((adim) => {
+  adimlar.forEach((adim) => {
     adimItem = {
       adim_id: adim.id,
       adim_sirasi: adim.adim_sirasi,
       adim_talimati: adim.adim_talimati,
+      icindekiler: [],
     };
     result.adimlar.push(adimItem);
   });
+
+  for (let i = 0; i < miktar.length; i++) {
+    for (let k = 0; k < result.adimlar.length; k++) {
+      if (miktar[i].adim_sirasi == result.adimlar[k].adim_sirasi) {
+        result.adimlar[k].icindekiler.push({
+          adimID: miktar[i].adim_id,
+          icindekiler_id: miktar[i].icindekiler_id,
+          icindekiler_adi: miktar[i].icindekiler_adi,
+          miktar: miktar[i].miktar,
+        });
+      }
+    }
+  }
+
   return result;
 }
 
